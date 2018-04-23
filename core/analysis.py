5f3ef3fa50ee0f2cb9c409a80a5e5a0aaf4284bb
Marco Aqil <marcoaqil@gmail.com>
2018-04-23 05:35:29 +0200

    Updates

--- core/analysis.py
+++ core/analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+from scipy import special
 import matplotlib.pyplot as plt
 import h5py
 import os
@@ -7,7 +8,7 @@ import os
 ####################################################################################################
 ####################################################################################################
 def GraphKernel(x,t,type='Gaussian'):
-    if t<=0:
+    if t<0:
         print("Need positive kernel parameter")
         return
     else:
@@ -18,6 +19,32 @@ def GraphKernel(x,t,type='Gaussian'):
         elif type=='Pyramid':
             return (np.sinc(np.sqrt(x)))**2
 
+
+####################################################################################################
+####################################################################################################
+def one_dim_Laplacian_eigenvalues(gridsize, h, syn=0):
+    diagonals = [-np.ones(gridsize-1),2*np.ones(gridsize+1),-np.ones(gridsize-1)]
+    Laplacian = sp.sparse.diags(diagonals,[-1,0,1]).toarray()
+        
+        #closed boundaries
+    Laplacian[0,0]=1
+    Laplacian[gridsize-1,gridsize-1]=1
+        
+    for p in range(syn):
+        k1=int(np.floor(gridsize*np.random.rand()))
+        k2=int(np.floor(gridsize*np.random.rand()))
+        Laplacian[k1,k2]=-1
+        Laplacian[k2,k1]=-1
+        Laplacian[k1,k1]+=1
+        Laplacian[k2,k2]+=1
+            
+    #periodic boundary
+    #Laplacian[0,gridsize-1]=-1
+    #Laplacian[gridsize-1,0]=-1
+    Laplacian/=(h**2)
+        
+    return np.linalg.eigvalsh(Laplacian)
+        
 ####################################################################################################
 ####################################################################################################
 ####################################################################################################
@@ -26,15 +53,19 @@ def GraphKernel(x,t,type='Gaussian'):
 #thresholding unique steady state if norm(x1-x2)<0.01    
 def H_Simple_Steady_State(alpha_EE=1, alpha_IE=1, alpha_EI=1, alpha_II=1, d_e=1, d_i=1, P=0, Q=0):
     #generate multiple initial conditions to find all steady states
-    initial_guesses = 8
+    initial_guesses = 10
+    #print("%.3g %.3g %.3g %.3g %.3g %.3g %.3g %.3g"%(alpha_EE, alpha_IE, alpha_EI, alpha_II, d_e, d_i, P, Q))
+
     x0 = np.zeros((2,initial_guesses))
-    x0[:,0] = np.array([1/(2*d_e), 1/(2*d_i)])
-    x0[:,1] = np.random.rand(2)
+    x0[:,1] = np.array([1/(2*d_e), 1/(2*d_i)])
     x0[:,2] = np.random.rand(2)
-    x0[:,3] = 5*np.random.rand(2)
-    x0[:,4] = 10*np.random.rand(2)
-    x0[:,5] = np.ones(2)
-    x0[:,6] = 2*np.ones(2)
+    x0[:,3] = np.random.rand(2)
+    x0[:,4] = 5*np.random.rand(2)
+    x0[:,5] = 10*np.random.rand(2)
+    x0[:,6] = np.ones(2)
+    x0[:,7] = 2*np.random.rand(2)
+    x0[:,8] = 20*np.random.rand(2)
+    x0[:,9] = 20*np.ones(2)
     
     success = False
     results = np.zeros((2,initial_guesses))
@@ -49,40 +80,50 @@ def H_Simple_Steady_State(alpha_EE=1, alpha_IE=1, alpha_EI=1, alpha_II=1, d_e=1,
     
     for i in range(initial_guesses):
         steady_state = sp.optimize.fsolve(f,x0[:,i],args=(alpha_EE,alpha_IE,alpha_EI,alpha_II,d_e,d_i),
-                                          xtol=1e-6, full_output=True)
-        if steady_state[0][0]>=0 and steady_state[0][1]>=0:
+                                          xtol=1e-9, full_output=True)
+        
+     #important line: conditions for success of SS calculation
+        if steady_state[0][0]>=0 and steady_state[0][1]>=0 and np.linalg.norm(steady_state[1]['fvec'],ord=1)<=1e-30:
             results[0,i]=steady_state[0][0]
             results[1,i]=steady_state[0][1]
             success=True
-    
-    #select and importantly sort the unique results    
-    uniques = np.unique(results, axis=1)
-    
-    #Further routine to select unique steady states up to some user-specified numerical tolerance   
-    tolerance=0.01
-    p=0
-    countSS=1
-    countoccurr=1
-    finals=np.copy(uniques)
-    
-    while p < len(uniques[0])-1:
-        diff = np.linalg.norm(uniques[:,p]-uniques[:,p+1], ord=2)
-        if diff < tolerance:
-            finals[:,p]+=uniques[:,p+1]
-            uniques=np.delete(uniques, p+1, axis=1)
-            finals=np.delete(finals,p+1,axis=1)
-            countoccurr+=1
         else:
-            finals[:,p]/=countoccurr
-            countoccurr=1
-            countSS+=1
-            p+=1
-                
-    finals[:,p]/=countoccurr        
+            results[0,i]= np.nan
+            results[1,i]= np.nan
     
-    print(str(len(finals[0]))+" unique steady states were found")        
-   
-    return finals, success
+    #select and importantly sort the unique, acceptable results
+    if success==True:
+        results = results[:,~np.all(np.isnan(results), axis=0)]    
+        uniques = np.unique(results, axis=1)
+        
+        #Further routine to select unique steady states up to some user-specified numerical tolerance   
+        tolerance=0.01
+        p=0
+        countSS=1
+        countoccurr=1
+        finals=np.copy(uniques)
+        
+        while p < len(uniques[0])-1:
+            diff = np.linalg.norm(uniques[:,p]-uniques[:,p+1], ord=2)
+            if diff < tolerance:
+                finals[:,p]+=uniques[:,p+1]
+                uniques=np.delete(uniques, p+1, axis=1)
+                finals=np.delete(finals,p+1,axis=1)
+                countoccurr+=1
+            else:
+                finals[:,p]/=countoccurr
+                countoccurr=1
+                countSS+=1
+                p+=1
+                    
+        finals[:,p]/=countoccurr        
+        
+    #    print(str(len(finals[0]))+" unique steady states were found")        
+       
+        return finals, success
+    else:
+  #      print("No positive, exact solutions were found")
+        return None, success
 
 ####################################################################################################
 ####################################################################################################
@@ -98,9 +139,12 @@ def GraphWC_Jacobian_TrDet(Laplacian_eigenvalues, Graph_Kernel='Gaussian', Ess=N
     t_IE = (0.5*sigma_IE**2)/D
     t_EI = (0.5*sigma_EI**2)/D    
     t_II = (0.5*sigma_II**2)/D
+        
     eigs=Laplacian_eigenvalues
     
-    Jacobian_eigenvalues=np.zeros((len(eigs),2))
+    Jacobian_eigenvalues=np.zeros((len(eigs),2),dtype=complex)
+    SStype=0
+    suitable=False
 
     
     if Ess == None or Iss == None:
@@ -117,10 +161,42 @@ def GraphWC_Jacobian_TrDet(Laplacian_eigenvalues, Graph_Kernel='Gaussian', Ess=N
     
     Determinant = -alpha_EE*alpha_II*a*b*GraphKernel(eigs,t_EE+t_II,type=Graph_Kernel)/(tau_e*tau_i) - alpha_EE*a*d_i*GraphKernel(eigs,t_EE,type=Graph_Kernel)/(tau_e*tau_i) + alpha_II*b*d_e*GraphKernel(eigs,t_II,type=Graph_Kernel)/(tau_e*tau_i) + alpha_IE*alpha_EI*a*b*GraphKernel(eigs,t_EI+t_IE,type=Graph_Kernel) + d_e*d_i/(tau_e*tau_i)
     
-    Jacobian_eigenvalues[:,0]= (Trace + np.sqrt(Trace**2 - 4*Determinant))/2
-    Jacobian_eigenvalues[:,1]= (Trace - np.sqrt(Trace**2 - 4*Determinant))/2
     
-    return Trace, Determinant, Jacobian_eigenvalues
+    Jacobian_eigenvalues[:,0]= (Trace + sp.sqrt(Trace**2 - 4*Determinant))/2.0
+    Jacobian_eigenvalues[:,1]= (Trace - sp.sqrt(Trace**2 - 4*Determinant))/2.0
+    
+    
+    if np.any(Jacobian_eigenvalues.real>0.1) or np.any(np.isnan(Jacobian_eigenvalues)):
+       # print("E*=%.4g, I*=%.4g: unstable"%(Ess,Iss))
+        SStype=0
+        suitable = False
+    else:
+        if np.all(Jacobian_eigenvalues.real<0) and np.all(Jacobian_eigenvalues.imag==0):
+           #print("E*=%.4g, I*=%.4g: strictly stable"%(Ess,Iss))
+            SStype=1
+            suitable = True
+      #all or any in the line below for imaginary? ask rikkert #do they all need imaginary parts?
+        elif np.all(Jacobian_eigenvalues.real<0) and np.any(Jacobian_eigenvalues.imag != 0):
+         #print("E*=%.4g, I*=%.4g: stable, with nonzero imaginary components"%(Ess,Iss))
+            SStype=2
+            suitable = True
+                #same question here. what if some imaginary are zero and some nonzero?
+        elif np.all(Jacobian_eigenvalues.real==0) and np.all(Jacobian_eigenvalues.imag != 0):
+          #  print("E*=%.4g, I*=%.4g: all purely imaginary eigenvalues (potential Hopf)"%(Ess,Iss))
+            SStype=3
+            suitable = True
+          
+        else:
+ 
+#cases with BOTH zero AND (negative) nonzero real components of eigens. 
+#first subcase: all imaginary parts are zero (stable/undetermined?)
+#second subcase: some imaginary parts are nonzero (need to consider overlap? does it matter if there are eigenvalues with magnitude zero?)
+#third subcase: all imaginary parts are nonzero (same question)
+         #   print("E*=%.3g, I*=%.3g: undetermined"%(Ess,Iss))
+            SStype=4
+            suitable = True
+        
+    return SStype, suitable #Trace, Determinant, Jacobian_eigenvalues
 
 
 ####################################################################################################
@@ -202,26 +278,26 @@ def Full_Analysis(Parameters, Laplacian_eigenvalues, Graph_Kernel, True_Spectrum
     sigma_IE=Parameters[9]
     sigma_EI=Parameters[10]
     sigma_II=Parameters[11]
-    #D=Parameters[12]
-    #tau_e=Parameters[13] 
-    #tau_i=Parameters[14] 
-    #sigma_noise_e=Parameters[15] 
-    #sigma_noise_i=Parameters[16]
+    D=Parameters[12]
+    tau_e=Parameters[13] 
+    tau_i=Parameters[14] 
+    sigma_noise_e=Parameters[15] 
+    sigma_noise_i=Parameters[16]
     
-    D=1.0
-    tau_e=1.0
-    tau_i=1.0
-    sigma_noise_e=1.0
-    sigma_noise_i=1.0
+    #D=1.0
+    #tau_e=1.0
+    #tau_i=1.0
+    #sigma_noise_e=1.0
+    #sigma_noise_i=1.0
     
     eigs=Laplacian_eigenvalues
     
     last_k=first_k+len(True_Spectrum)
     
-    
+    success = False
     #beginning of calculations
     steady_states, success = H_Simple_Steady_State(alpha_EE, alpha_IE, alpha_EI, alpha_II, d_e, d_i, P, Q)
-    found_stable = False
+    found_suitable = False
     
     if success==True:
         nrSS=len(steady_states[0])
@@ -237,28 +313,15 @@ def Full_Analysis(Parameters, Laplacian_eigenvalues, Graph_Kernel, True_Spectrum
             Ess = steady_states[0,ss]
             Iss = steady_states[1,ss]
             
+            
             if LSA==True:
-                Tr, Det, JacEigs = GraphWC_Jacobian_TrDet(eigs, Graph_Kernel, Ess, Iss,                        
+                SStypes[ss], found_suitable = GraphWC_Jacobian_TrDet(eigs, Graph_Kernel, Ess, Iss,                        
                                                  alpha_EE, alpha_IE, alpha_EI, alpha_II, d_e, d_i,
                                                  sigma_EE, sigma_IE, sigma_EI, sigma_II, D, 
                                                  tau_e, tau_i)    
                 
-                if np.all(JacEigs.real<=0):
-                    print("Stable")
-                    SStypes[ss]=1
-                    found_stable = True
+              
                     
-                else:
-                   
-                    #if np.all(Tr**2 - 4*Det >=0):
-                    if np.all(JacEigs.imag == 0):
-                        print("Unstable: no oscillations")
-                        SStypes[ss]=2
-                    else:
-                        print("Unstable: potential Hopf")
-                        SStypes[ss]=3
-            
-                          
     
             allG[ss,:,:,:] = Graph_WC_SpatialPowerSpectrum(eigs, Graph_Kernel, Ess, Iss, 
                                               alpha_EE, alpha_IE, alpha_EI, alpha_II, d_e, d_i,
@@ -270,83 +333,89 @@ def Full_Analysis(Parameters, Laplacian_eigenvalues, Graph_Kernel, True_Spectrum
             #important: insert here a metric to quantify distance between true spectrum and calculated
             ######****######
             #Dist[ss] = np.max(np.abs(G[first_k:last_k,0,0]- True_Spectrum))
+            #scale_param = np.dot(allG[ss,first_k:last_k,0,0],True_Spectrum)/np.linalg.norm(True_Spectrum, ord=2)
             Dist[ss] = np.linalg.norm(allG[ss,first_k:last_k,0,0]- True_Spectrum, ord=2)
             #Dist[ss] = -stats.ks_2samp( Gmatrix[first_k:last_k,0,0], True_Spectrum )[1]
         
-        if found_stable==True:
-            SSS = np.where(SStypes==1)
-            bestSSS = np.nanargmin(Dist[SSS])
-            minDist=Dist[bestSSS]
-            print("Best (stable) steady state: "+str(bestSSS)+", with Ess="+str(round(steady_states[0,bestSSS],3))+" Iss="+str(round(steady_states[1,bestSSS],3))+", Distance: "+str(round(minDist,3)))       
-            bestG=allG[bestSSS,:,:,:]
+        if found_suitable==True:    
             
-            
+            mask = np.argwhere(SStypes!=0)       
+            if ~np.all(np.isnan(Dist[mask])):
+                bestSSS = mask[np.nanargmin(Dist[mask])][0]
+                minDist=Dist[bestSSS]
+                print("Best suitable steady state: "+str(bestSSS)+", with Ess="+str(round(steady_states[0,bestSSS],3))+" Iss="+str(round(steady_states[1,bestSSS],3))+", Distance: "+str(round(minDist,3)))       
+                bestG=allG[bestSSS,:,:,:]
                 
-            plt.ioff()
-            if Visual==True:
-                plt.ion()
-                fig = plt.figure()
-                #ax = fig.add_subplot(111)
-                #ax.set_xlim(-0.1, 20000)
-                #ax.set_ylim(0, 20)
-                line2, = plt.loglog(np.arange(1,len(eigs)+1),bestG[:,1,1], 'b-')
-                line1, = plt.loglog(np.arange(1,len(eigs)+1),bestG[:,0,0], 'r-')
-                line3, = plt.loglog(np.arange(first_k+1,last_k+1),True_Spectrum, 'k--')
                 
-              
-                
-            
-           
-            if SaveFiles==True:
-                
-                if Filepath==' ':
-                    filepath = 'G:/Macbook Stuff/Analysis Results/'+Graph_Kernel+' Kernel/aEE=%.3g aIE=%.3g aEI=%.3g aII=%.3g dE=%.3g dI=%.3g ' %(alpha_EE,alpha_IE,alpha_EI,alpha_II,d_e,d_i)
-                    filepath += 'P=%.3g Q=%.3g sEE=%.3g sIE=%.3g sEI=%.3g sII=%.3g D=%.3g tE=%.3g tI=%.3g snE=%.3g snI=%.3g/'%(P,Q,sigma_EE,sigma_IE,sigma_EI,sigma_II,D,tau_e,tau_i,sigma_noise_e,sigma_noise_i) 
-                else:
-                    filepath=Filepath
                     
-                if not os.path.exists(filepath):
-                    os.makedirs(filepath)
-                
-                file = open(filepath+'Parameters.dat', 'w+')        
-                file.write("Alpha_EE=%f \n"%alpha_EE)
-                file.write("Alpha_IE=%f \n"%alpha_IE)
-                file.write("Alpha_EI=%f \n"%alpha_EI)
-                file.write("Alpha_II=%f \n"%alpha_II)
-                file.write("d_E=%f \n"%d_e)
-                file.write("d_I=%f \n"%d_i)
-                file.write("P=%f \n"%P)
-                file.write("Q=%f \n"%Q)
-                file.write("Sigma_EE=%f \n"%sigma_EE)
-                file.write("Sigma_IE=%f \n"%sigma_IE)
-                file.write("Sigma_EI=%f \n"%sigma_EI)
-                file.write("Sigma_II=%f \n"%sigma_II)
-                file.write("D=%f \n"%D)
-                file.write("Tau_E=%f \n"%tau_e)
-                file.write("Tau_I=%f \n"%tau_i)
-                file.write("Sigma_Noise_E=%f \n"%sigma_noise_e)
-                file.write("Sigma_Noise_I=%f \n"%sigma_noise_i)       
-                file.close
-                
-                with h5py.File(filepath+"analysis.h5") as hf:
-                    hf.create_dataset("Steady States",  data=steady_states)
-                    hf.create_dataset("Distance",  data=Dist)
-                    hf.create_dataset("Type",  data=SStypes)
-                    hf.create_dataset("AllG", data=allG)
-                
+                plt.ioff()
                 if Visual==True:
-                    plt.savefig(filepath+"Power Spectrum.png")   
-                
+                    plt.ion()
+                    fig = plt.figure()
+                    #ax = fig.add_subplot(111)
+                    #ax.set_xlim(-0.1, 20000)
+                    #ax.set_ylim(0, 20)
+                    line2, = plt.loglog(np.arange(1,len(eigs)+1),bestG[:,1,1], 'b-')
+                    line1, = plt.loglog(np.arange(1,len(eigs)+1),bestG[:,0,0], 'r-')
+                    line3, = plt.loglog(np.arange(first_k+1,last_k+1),True_Spectrum, 'k--')
                     
-            #if G[3,0,0]-G[-3,0,0]<=1:
-            # minDist=10000*np.random.rand()+1000
-            
-                               
-            return minDist
+                  
+                    
+                
+               
+                if SaveFiles==True:
+                    
+                    if Filepath==' ':
+                        filepath = 'G:/Macbook Stuff/Analysis Results/'+Graph_Kernel+' Kernel/aEE=%.3g aIE=%.3g aEI=%.3g aII=%.3g dE=%.3g dI=%.3g ' %(alpha_EE,alpha_IE,alpha_EI,alpha_II,d_e,d_i)
+                        filepath += 'P=%.3g Q=%.3g sEE=%.3g sIE=%.3g sEI=%.3g sII=%.3g D=%.3g tE=%.3g tI=%.3g snE=%.3g snI=%.3g/'%(P,Q,sigma_EE,sigma_IE,sigma_EI,sigma_II,D,tau_e,tau_i,sigma_noise_e,sigma_noise_i) 
+                    else:
+                        filepath=Filepath
+                        
+                    if not os.path.exists(filepath):
+                        os.makedirs(filepath)
+                    
+                    file = open(filepath+'Parameters.dat', 'w+')        
+                    file.write("Alpha_EE=%f \n"%alpha_EE)
+                    file.write("Alpha_IE=%f \n"%alpha_IE)
+                    file.write("Alpha_EI=%f \n"%alpha_EI)
+                    file.write("Alpha_II=%f \n"%alpha_II)
+                    file.write("d_E=%f \n"%d_e)
+                    file.write("d_I=%f \n"%d_i)
+                    file.write("P=%f \n"%P)
+                    file.write("Q=%f \n"%Q)
+                    file.write("Sigma_EE=%f \n"%sigma_EE)
+                    file.write("Sigma_IE=%f \n"%sigma_IE)
+                    file.write("Sigma_EI=%f \n"%sigma_EI)
+                    file.write("Sigma_II=%f \n"%sigma_II)
+                    file.write("D=%f \n"%D)
+                    file.write("Tau_E=%f \n"%tau_e)
+                    file.write("Tau_I=%f \n"%tau_i)
+                    file.write("Sigma_Noise_E=%f \n"%sigma_noise_e)
+                    file.write("Sigma_Noise_I=%f \n"%sigma_noise_i)       
+                    file.close
+                    
+                    with h5py.File(filepath+"analysis.h5") as hf:
+                        hf.create_dataset("Steady States",  data=steady_states)
+                        hf.create_dataset("Distance",  data=Dist)
+                        hf.create_dataset("Type",  data=SStypes)
+                        hf.create_dataset("AllG", data=allG)
+                    
+                    if Visual==True:
+                        plt.savefig(filepath+"Power Spectrum.png")   
+                    
+                        
+                #if G[3,0,0]-G[-3,0,0]<=1:
+                # minDist=10000*np.random.rand()+1000
+                
+                                   
+                return minDist
+            else:
+                #print("Unrealistic spectra")
+                return float('Inf')
         else:
-            print("No stable steady states found")
+            #print("No suitable (LSA) steady states found")
             return float('Inf')
     
     else:
-        print("No positive steady states found")
+        #case where no positive/exact solutions found
         return float('Inf')
--- core/simulation.py
+++ core/simulation.py
@@ -13,7 +13,7 @@ from numba import jit
 #from sympy import exp
 from scipy import stats
 import os
-#from analysis import GraphKernel
+from analysis import GraphKernel
 
 ####################################################################################################
 ####################################################################################################    
@@ -21,7 +21,7 @@ import os
 ####################################################################################################
 ####################################################################################################    
 ####################################################################################################   
-#DIFFUSION ON A CHAIN GRAPH (analogous to 1D finite difference)
+#testing any time-evolution propagator defined by a kernel on the graph
 ####################################################################################################   
 
 def graph_propagator_test(u_0, Graph_Kernel='Gaussian', t=1, 
@@ -69,7 +69,7 @@ def graph_propagator_test(u_0, Graph_Kernel='Gaussian', t=1,
 ##GRAPH STOCHASTIC WILSON COWAN
 ##################################################################
 #compute the four diffusion operators beforehand
-def WC_graph_propagators(Graph_Kernel='Gaussian',
+def graph_WCM_propagators(Graph_Kernel='Gaussian',
                        alpha_EE=1, alpha_IE=1, alpha_EI=1, alpha_II=1,
                        sigma_EE=10, sigma_IE=10, sigma_EI=10, sigma_II=10, D=1,
                        one_dim=False, syn=0, gridsize=1000, h=0.01,
@@ -79,6 +79,8 @@ def WC_graph_propagators(Graph_Kernel='Gaussian',
     t_IE = (0.5*sigma_IE**2)/D
     t_EI = (0.5*sigma_EI**2)/D    
     t_II = (0.5*sigma_II**2)/D
+    
+    ForceParallel=True
     
     if one_dim==True:
         diagonals = [-np.ones(gridsize-1),2*np.ones(gridsize+1),-np.ones(gridsize-1)]
@@ -106,34 +108,46 @@ def WC_graph_propagators(Graph_Kernel='Gaussian',
         
     else:
         s = eigvals
+        U = eigvecs
         V = eigvecs.T
-        
+           
      
-    
-    diag_prop_EE = alpha_EE * GraphKernel(s, t_EE, Graph_Kernel)
-    diag_prop_IE = alpha_IE * GraphKernel(s, t_IE, Graph_Kernel)
-    diag_prop_EI = alpha_EI * GraphKernel(s, t_EI, Graph_Kernel)
-    diag_prop_II = alpha_II * GraphKernel(s, t_II, Graph_Kernel)
-    
-    mask_EE = np.flatnonzero(diag_prop_EE)
-    mask_IE = np.flatnonzero(diag_prop_IE)
-    mask_EI = np.flatnonzero(diag_prop_EI)
-    mask_II = np.flatnonzero(diag_prop_II)
-    
-    EE_skip = diag_prop_EE[mask_EE]
-    IE_skip = diag_prop_IE[mask_IE]
-    EI_skip = diag_prop_EI[mask_EI]
-    II_skip = diag_prop_II[mask_II]
+    if ForceParallel==True:   
+        diag_prop_EE = alpha_EE * GraphKernel(s, t_EE, Graph_Kernel)
+        diag_prop_IE = alpha_IE * GraphKernel(s, t_IE, Graph_Kernel)
+        diag_prop_EI = alpha_EI * GraphKernel(s, t_EI, Graph_Kernel)
+        diag_prop_II = alpha_II * GraphKernel(s, t_II, Graph_Kernel)  
+         
+        mask_EE = np.flatnonzero(diag_prop_EE)
+        mask_IE = np.flatnonzero(diag_prop_IE)
+        mask_EI = np.flatnonzero(diag_prop_EI)
+        mask_II = np.flatnonzero(diag_prop_II)
         
-    prop_EEV = EE_skip[:,None] * V[mask_EE,:]
-    prop_IEV = IE_skip[:,None] * V[mask_IE,:]
-    prop_EIV = EI_skip[:,None] * V[mask_EI,:]
-    prop_IIV = II_skip[:,None] * V[mask_II,:]
-       
-    propagator_EE = transpose_parallel_dot(V[mask_EE,:], prop_EEV)     #np.dot(U, np.dot(s_exp_matrix_EE,V))  
-    propagator_IE = transpose_parallel_dot(V[mask_IE,:], prop_IEV)
-    propagator_EI = transpose_parallel_dot(V[mask_EI,:], prop_EIV)
-    propagator_II = transpose_parallel_dot(V[mask_II,:], prop_IIV)
+        EE_skip = diag_prop_EE[mask_EE]
+        IE_skip = diag_prop_IE[mask_IE]
+        EI_skip = diag_prop_EI[mask_EI]
+        II_skip = diag_prop_II[mask_II]
+            
+        prop_EEV = EE_skip[:,None] * V[mask_EE,:]
+        prop_IEV = IE_skip[:,None] * V[mask_IE,:]
+        prop_EIV = EI_skip[:,None] * V[mask_EI,:]
+        prop_IIV = II_skip[:,None] * V[mask_II,:]
+           
+        propagator_EE = transpose_parallel_dot(V[mask_EE,:], prop_EEV)     #np.dot(U, np.dot(s_exp_matrix_EE,V))  
+        propagator_IE = transpose_parallel_dot(V[mask_IE,:], prop_IEV)
+        propagator_EI = transpose_parallel_dot(V[mask_EI,:], prop_EIV)
+        propagator_II = transpose_parallel_dot(V[mask_II,:], prop_IIV)
+    else:
+        diag_prop_EE = sp.sparse.diags(alpha_EE * GraphKernel(s, t_EE, Graph_Kernel)).toarray()
+        diag_prop_IE = sp.sparse.diags(alpha_IE * GraphKernel(s, t_IE, Graph_Kernel)).toarray()
+        diag_prop_EI = sp.sparse.diags(alpha_EI * GraphKernel(s, t_EI, Graph_Kernel)).toarray()
+        diag_prop_II = sp.sparse.diags(alpha_II * GraphKernel(s, t_II, Graph_Kernel)).toarray()
+        
+        propagator_EE = np.dot(U, np.dot(diag_prop_EE,V))  
+        propagator_IE = np.dot(U, np.dot(diag_prop_IE,V))
+        propagator_EI = np.dot(U, np.dot(diag_prop_EI,V))
+        propagator_II = np.dot(U, np.dot(diag_prop_II,V))
+        
     
     return propagator_EE, propagator_IE, propagator_EI, propagator_II
  
@@ -142,7 +156,7 @@ def transpose_parallel_dot(A, B):
     return np.dot(A.T, B)
 
 @jit(nopython=True, parallel=True)    
-def WCLoop(E_0, I_0,  Delta_t,
+def GWCM_Loop(E_0, I_0,  Delta_t,
            propagator_EE, propagator_IE, propagator_EI, propagator_II, 
            d_e, d_i, P, Q, tau_e, tau_i, Noise_E, Noise_I):
     
@@ -165,15 +179,21 @@ def Graph_Wilson_Cowan_Model(E_0, I_0, Time, Delta_t,
         ax = fig.add_subplot(111)
         ax.set_xlim(0, len(E_0))
         ax.set_ylim(0, 1)
-        line2, = ax.plot(np.arange(len(I_0)), I_0, 'b-')
-        line1, = ax.plot(np.arange(len(E_0)), E_0, 'r-')
-        
+        #line2, = ax.plot(np.arange(len(I_0)), I_0, 'b-')
+        #line1, = ax.plot(np.arange(len(E_0)), E_0, 'r-')
+        print(str(E_0[10])+" "+str(I_0[20]))
+        ax.plot(I_0, 'b-')
+        ax.plot(E_0, 'r-')
+        fig.canvas.draw()
      
     for i in range(int(round(Time/Delta_t))):
         print(i)
-        #currently SAME noise for excitatory and inhibitory
-        Noise_E = sigma_noise_e * np.array([gauss(0.0, 1.0) for k in range(len(E_0))])
-        Noise_I = sigma_noise_i * np.array([gauss(0.0, 1.0) for k in range(len(I_0))])
+        if sigma_noise_e!=0 or sigma_noise_i!=0:
+            Noise_E = sigma_noise_e * np.array([gauss(0.0, 1.0) for k in range(len(E_0))])
+            Noise_I = sigma_noise_i * np.array([gauss(0.0, 1.0) for k in range(len(I_0))])
+        else:
+            Noise_E = 0
+            Noise_I = 0
         #it turns out that the alphas ARE important, ie. at least manually, i cant get oscillations if i set them to one
         #simply manipulating the sigmas doesnt appear to be enough for oscillations. analysis or systematic numerics would solve this
         #impulse response    
@@ -181,19 +201,25 @@ def Graph_Wilson_Cowan_Model(E_0, I_0, Time, Delta_t,
         #     E_Delta_t[600:650]=0.9*np.ones(50)
         #     I_Delta_t[580:620]=0.9*np.ones(40)
         
-        E_Delta_t, I_Delta_t = WCLoop(E_0, I_0, 
+        E_Delta_t, I_Delta_t = GWCM_Loop(E_0, I_0, Delta_t,
            propagator_EE, propagator_IE, propagator_EI, propagator_II, 
-                          Delta_t, d_e, d_i, P, Q, tau_e, tau_i, Noise_E, Noise_I)
+                           d_e, d_i, P, Q, tau_e, tau_i, Noise_E, Noise_I)
             
-        E_0 = np.copy(E_Delta_t)   
-        I_0 = np.copy(I_Delta_t)
-        
+
         if Visual==True and i%10 == 0:
-            line2.set_ydata(I_Delta_t)
-            line1.set_ydata(E_Delta_t)
             
+            ax.clear()
+            #line2.set_ydata(I_Delta_t)
+            #line1.set_ydata(E_Delta_t)
+            ax.plot(I_Delta_t, 'b-')
+            ax.plot(E_Delta_t, 'r-')
             fig.canvas.draw()
             fig.canvas.flush_events()
-               
+           
+        E_0 = np.copy(E_Delta_t)   
+        I_0 = np.copy(I_Delta_t)
+        print(str(E_0[10])+" "+str(I_0[20]))
 
+            
+            
     return E_Delta_t, I_Delta_t
